1f611bf12da46123f76a7fcbf40eb332877d8c73
André Soares <andreiarley@gmail.com>
2025-09-17 21:21:28 -0300

    add outpul

--- src/VND.cpp
+++ src/VND.cpp
@@ -20,18 +20,19 @@ void VND::vnd(std::vector<std::vector<RouteStep>> &solution, node** &matrix, int
             //std::cout << "\nTwoOPT funcionou: " << total_cost << std::endl;
         }
 
-        if(swap2.run(solution, vehicle_capacity, total_cost)) {
-            funcionou = true;
-            total_cost = utils.custo_total(solution);
-            // std::cout << "\nSWAP2 funcionou: " << total_cost << std::endl;
-        }
-
         if(reInsertion.run(solution, vehicle_capacity)) {
             funcionou = true;
             total_cost = utils.custo_total(solution);
             // std::cout << "\nREINSERTION funcionou: " << total_cost << std::endl;
         }
 
+        if(swap2.run(solution, vehicle_capacity, total_cost)) {
+            funcionou = true;
+            total_cost = utils.custo_total(solution);
+            // std::cout << "\nSWAP2 funcionou: " << total_cost << std::endl;
+        }
+
+        
         count++;
         //std::cout << "\nVND loop: " << count << std::endl;
 
